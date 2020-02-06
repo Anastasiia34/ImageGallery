241ed2b8abe5da3c1dd300b048f9b7ca998d193a
Anastasiia34 <mainstreamainstream@gmail.com>
2020-02-06 12:16:19 +0300

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Анастасия Стрекалова on 06.02.2020.
+//  Copyright © 2020 Анастасия Стрекалова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
